8fe232171ab4c9999526ca1a0dd4a1a1237daf09
Zjaaadac Zjaaadacowski <zjaaadac@example123.com>
2023-12-15 16:00:21 +0100

    Commit2

--- src/main/java/pl/zajavka/Main.java
+++ src/main/java/pl/zajavka/Main.java
@@ -3,5 +3,7 @@ package pl.zajavka;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Warsztat 12_1 ");
+        System.out.println("Test commit 2 ");
+
     }
 }
